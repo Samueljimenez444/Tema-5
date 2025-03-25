8173b088dd054f94c8fd53f9c4c11833f62730a6
Samueljimenez444 <samuel.jimenez@iesnervion.es>
2025-03-25 14:39:06 +0100

    Ejercicio Animales

--- Tema5/src/ejercicio3Parte3/Gato.java
+++ Tema5/src/ejercicio3Parte3/Gato.java
@@ -1,11 +1,61 @@
 package ejercicio3Parte3;
 
-public class Gato extends AnimalDomestico {
+import java.util.Random;
 
+public class Gato extends AnimalDomestico {
+	Random rand=new Random();
 	public Gato(String nombre, String color, int peso, String raza) {
 		
 		super(nombre, color, peso, raza);
 		
 	}
+
+	@Override
+	public boolean comer() {
+		boolean hambre;
+		hambre=rand.nextBoolean();
+		return hambre;
+	}
+
+	@Override
+	public boolean dormir() {
+		boolean sueño;
+		sueño=rand.nextBoolean();
+		return sueño;
+	}
+
+	@Override
+	public String hacerRuido() {
+		boolean ruido;
+		String sonido;
+		ruido=rand.nextBoolean();
+		if(ruido) {
+			sonido="Miau";
+		}
+		else {
+			sonido="....";
+		}
+		return sonido;
+	}
+
+	public boolean hacerCaso() {
+		boolean caso;
+		int aleatorio;
+		aleatorio=rand.nextInt(1,101);
+		if(aleatorio<95) {
+			caso=false;
+		}
+		else {
+			caso=true;
+		}
+		return caso;
+	}
+	
+	public boolean toserBolaPelo() {
+		boolean toser;
+		toser=rand.nextBoolean();
+		return toser;
+	}
+	
 
 }
--- Tema5/src/ejercicio3Parte3/Perro.java
+++ Tema5/src/ejercicio3Parte3/Perro.java
@@ -1,11 +1,47 @@
 package ejercicio3Parte3;
 
-public class Perro extends AnimalDomestico {
+import java.util.Random;
 
+public class Perro extends AnimalDomestico {
+	Random rand=new Random();
 	public Perro(String nombre, String color, int peso, String raza) {
 		
 		super(nombre, color, peso, raza);
 
+	}
+
+	@Override
+	public boolean comer() {
+		boolean comer;
+		comer=rand.nextBoolean();
+		return comer;
+	}
+
+	@Override
+	public boolean dormir() {
+		boolean dormir;
+		dormir=rand.nextBoolean();
+		return dormir;
+	}
+
+	@Override
+	public String hacerRuido() {
+		boolean hacerRuido;
+		String ruido;
+		hacerRuido=rand.nextBoolean();
+		if(hacerRuido) {
+			ruido="Guau Guau";
+		}
+		else {
+			ruido="....";
+		}
+		return ruido;
+	}
+	
+	public boolean sacarPaseo() {
+		boolean salirPaseo;
+		salirPaseo=rand.nextBoolean();
+		return salirPaseo;
 	}
 
 }
